c795992428e0e53aab17be9302ee77162aee062b
Shatou136 <aishatoumohammed136@.com>
2025-07-25 02:24:10 -0600

    Initial commit

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
-export default function loginpage() {
-    return(
-        <div>
-            <h1>hello from the login page</h1>
-        </div> 
+"use client";
+import { auth } from "@/lib/auth";
+import { LoginForm } from "./_components/LoginForm";
+
+export default function Loginpage() { 
+const session= await auth.api.getsession
+
+    
+    return (
+        <LoginForm />;
     )
 }
